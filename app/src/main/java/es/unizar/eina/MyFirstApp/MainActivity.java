b999313d9b068d4aa1653cd4a1cca56b79ddd936
Patseho <juanv4004@gmail.com>
2019-10-02 15:44:30 +0200

    commit 1

---
+++ app/src/main/java/es/unizar/eina/MyFirstApp/MainActivity.java
@@ -0,0 +1,27 @@
+package es.unizar.eina.MyFirstApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "es.unizar.eina.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Llamado cuando el usuario pulsa el botón Send */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
